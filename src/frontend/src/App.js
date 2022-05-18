cfb44fb4de81d8ee6abda00f325b88f71b56887a
Dima <soloveid87@mail.ru>
2022-05-18 22:50:29 +1000

    fetch students

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button} from 'antd';
 import { getAllStudent } from "./client";
 
 function App() {
-    getAllStudent().then(console.log);
-    return (
-        <div className="App">
-            <Button type="primary">Hello</Button>
-        </div>
-    );
+    const [students, setStudents] = useState([]);
+    const fetchStudents = () =>
+        getAllStudent()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+            });
+
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    }, []);
+
+    if(students.length <=0){
+        return "no data";
+    }
+
+    return students.map((student, index) => {
+        return <p key={index}>{student.id} {student.name}</p>;
+    })
 }
 
 export default App;
