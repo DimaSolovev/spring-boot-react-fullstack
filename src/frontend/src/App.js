4908da304ccfcfeedac15615ae010ff875bb19b6
Dima <soloveid87@mail.ru>
2022-05-18 23:21:49 +1000

    layout doesnt work

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import { Button} from 'antd';
 import { getAllStudent } from "./client";
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
     const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+
     const fetchStudents = () =>
         getAllStudent()
             .then(res => res.json())
@@ -22,9 +32,55 @@ function App() {
         return "no data";
     }
 
-    return students.map((student, index) => {
-        return <p key={index}>{student.id} {student.name}</p>;
-    })
+    return <Layout
+                style={{
+                    minHeight: '100vh',
+                }}
+            >
+            <Sider collapsible collapsed={collapsed}
+                   onCollapse={setCollapsed}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" />
+            </Sider>
+                <Layout className="site-layout">
+                    <Header
+                        className="site-layout-background"
+                        style={{
+                            padding: 0,
+                        }}
+                    />
+                    <Content
+                        style={{
+                            margin: '0 16px',
+                        }}
+                    >
+                        <Breadcrumb
+                            style={{
+                                margin: '16px 0',
+                            }}
+                        >
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div
+                            className="site-layout-background"
+                            style={{
+                                padding: 24,
+                                minHeight: 360,
+                            }}
+                        >
+                            Bill is a cat.
+                        </div>
+                    </Content>
+                    <Footer
+                        style={{
+                            textAlign: 'center',
+                        }}
+                    >
+                        Ant Design ©2018 Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
 }
 
 export default App;
