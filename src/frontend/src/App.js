c586e63b93095d0e6f6fa3681fd040fd85e3499c
Dima <soloveid87@mail.ru>
2022-05-19 20:48:22 +1000

    sider work

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -9,7 +9,29 @@ import {
     TeamOutlined,
     UserOutlined,
 } from '@ant-design/icons';
+
 const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
 
 function App() {
     const [students, setStudents] = useState([]);
@@ -32,55 +54,56 @@ function App() {
         return "no data";
     }
 
-    return <Layout
-                style={{
-                    minHeight: '100vh',
-                }}
-            >
-            <Sider collapsible collapsed={collapsed}
-                   onCollapse={setCollapsed}>
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
                 <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
             </Sider>
-                <Layout className="site-layout">
-                    <Header
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
                         className="site-layout-background"
                         style={{
-                            padding: 0,
-                        }}
-                    />
-                    <Content
-                        style={{
-                            margin: '0 16px',
+                            padding: 24,
+                            minHeight: 360,
                         }}
                     >
-                        <Breadcrumb
-                            style={{
-                                margin: '16px 0',
-                            }}
-                        >
-                            <Breadcrumb.Item>User</Breadcrumb.Item>
-                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <div
-                            className="site-layout-background"
-                            style={{
-                                padding: 24,
-                                minHeight: 360,
-                            }}
-                        >
-                            Bill is a cat.
-                        </div>
-                    </Content>
-                    <Footer
-                        style={{
-                            textAlign: 'center',
-                        }}
-                    >
-                        Ant Design ©2018 Created by Ant UED
-                    </Footer>
-                </Layout>
+                        Bill is a cat.
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
             </Layout>
+        </Layout>
+    );
 }
 
 export default App;
