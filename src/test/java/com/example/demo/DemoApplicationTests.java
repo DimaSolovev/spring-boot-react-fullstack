2f731c3a316b5e464b1fcc5dbed3d9a94bd45ea9
Dima <soloveid87@mail.ru>
2022-05-25 15:07:48 +1000

    make test failed

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+		Assertions.fail("Oops test fail");
+    }
 
 }
